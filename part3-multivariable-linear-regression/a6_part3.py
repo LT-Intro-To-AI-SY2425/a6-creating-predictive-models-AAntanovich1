6fe3fdbd85a7ddeb3af02c34bfe6f430c4cd572d
Alan Antanovich <alanantanovich3@gmail.com>
2024-12-19 10:39:19 -0600

    Finished

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -9,14 +9,29 @@ x = data[["miles","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size= .2) 
 
 #create linear regression model
+model = LinearRegression().fit(xtrain, ytrain)
 
 #Find and print the coefficients, intercept, and r squared values. 
-#Each should be rounded to two decimal places. 
+#Each should be rounded to two decimal places.
+coef = np.around(model.coef_, 2)
+intercept = np.around(float(model.intercept_), 2)
+r_squared = np.around(model.score(x, y), 2)
 
+predict = model.predict(xtest)
+predict = np.around(predict, 2)
+print("R Squared value: ", r_squared)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
 print("***************")
 print("Testing Results")
+
+for index in range(len(xtest)):
+    miles = xtest[index]
+    actual = ytest[index]
+    predicted = predict[index]
+
+    print("Age: ", float(miles[1]),"Mileage: ", float(miles[0]), " Predicted price: ", predicted," Actual price: ", actual)
--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -10,18 +10,47 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
-
+print("Features (X):")
+print(x)
+print("\nTarget (Y):")
+print(y)
 # Step 2: Standardize the data using StandardScaler, 
-
-# Step 3: Transform the data
+scaler = StandardScaler().fit(x)
+x = scaler.transform(x)
 
 # Step 4: Split the data into training and testing data
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-# Step 5: Fit the data
+
+# Step 5: Fit the model
+model = linear_model.LogisticRegression(max_iter=1000).fit(x_train, y_train)
 
 # Step 6: Create a LogsiticRegression object and fit the data
+model = linear_model.LogisticRegression().fit(x_train, y_train)
 
 # Step 7: Print the score to see the accuracy of the model
-
+print("Accuracy:", model.score(x_test, y_test))
+print("*************")
+print("Testing Results:")
+print("")
+print(y_test)
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
+for index in range(len(x_test)):
+    x = x_test[index]
+    x = x.reshape(-1, 3)
+    print(x)
+    y_pred = int(model.predict(x))
+
+    if y_pred == 0:
+        y_pred = "Male"
+    else:
+        y_pred = "Female"
+    
+    actual = y_test[index]
+    if actual == 0:
+        actual = "Male"
+    else:
+        actual = "Female"
+    print("Predicted Gender: " + y_pred + " Actual Gender : " + actual)
+    print("")
